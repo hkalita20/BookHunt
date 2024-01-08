2d3d70999304a517904cdce5354da6e25c6da1fe
hkalita20 <hkalita830@gmail.com>
2024-01-08 21:04:32 +0530

    Initial Author Routes

---
+++ routes/authors.js
@@ -0,0 +1,64 @@
+const express=require("express");
+const Author = require("../models/author");
+const router=express.Router()
+
+
+router.get('/',async(req,res)=>{
+    let searchOptions={};
+    if(req.query.name !=null && req.name!=''){
+        searchOptions.name=new RegExp(req.query.name,'i')
+    }
+    try{
+        const authors=await Author.find(searchOptions);
+        res.render('authors/index',{authors:authors,
+            searchOptions:req.query})
+    }
+    catch{
+        res.redirect('/');
+    }
+})
+
+
+//new Author Route
+router.get('/new',(req,res)=>{
+    res.render('authors/new',{author:new Author()})
+})
+
+
+//Create Author Route
+router.post('/',async(req,res)=>{
+    const author=new Author({
+        name:req.body.name
+    })
+
+    try{
+
+        const newAuthor=await author.save();
+
+        // author.save((err,newAuthor)=>{
+            //     if(err){
+            //         res.render('authors/new',{
+            //             author:author,
+            //             errorMessage: "Error creating Author"
+            //         })
+            //     }
+    }catch{
+     res.render('authors/new',{author:author,
+    errorMessage:'Error creating Author'})   
+    }
+    
+
+
+
+    //
+    //     else{
+    //         // res.redirect('authors/${newAuthor.id}')
+    //         res.redirect('authors')
+    //     }
+    // })
+    // res.send(req.body.name)
+})
+
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -7,13 +7,15 @@ const mongoose=require("mongoose");
 const app=express();
 const expressLayouts=require('express-ejs-layouts');
 const indexRouter=require('./routes/index');
-
+const authorRouter=require('./routes/authors')
+const bodyParser=require("body-parser");
 
 app.set('view engine','ejs')
 app.set('views',__dirname+'/views')
 app.set('layout','layouts/layout')
 app.use(expressLayouts);
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({limit:'10mb',extended:false}))
 
 
 
@@ -26,4 +28,5 @@ db.once('open',() => console.log("Connected to Mongoose"))
 
 
 app.use('/',indexRouter);
+app.use('/authors',authorRouter);
 app.listen(process.env.PORT || 5000)
